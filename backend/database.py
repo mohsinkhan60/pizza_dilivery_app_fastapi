d53eaa415e841e778d5142eacb4471217ec509d5
mohsinkhan60 <mohsinkhan.contact0@gmail.com>
2025-07-06 01:31:22 +0500

    update database connection string and add user and order models

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-engine=create_engine('postgresql://postgres:nathanoj35@localhost/pizza_delivery',
+engine=create_engine('postgresql://postgres:Mohsin123khan@localhost/pizza_delivery',
     echo=True
 )
 
---
+++ backend/models.py
@@ -0,0 +1,45 @@
+from database import Base, engine, Session
+from sqlalchemy import Column, Integer, String, Boolean, Text, ForeignKey
+from sqlalchemy_utils.types import ChoiceType
+from sqlalchemy.orm import relationship
+
+class User(Base):
+    __tablename__ = 'user'
+    
+    id = Column(Integer, primary_key=True)
+    username = Column(String(25), nullable=False)
+    email = Column(String(80), unique=True, nullable=False)
+    password = Column(Text, nullable=False)
+    is_staff = Column(Boolean, default=False)
+    is_active = Column(Boolean, default=True)
+    orders = relationship('Order', back_populates='user')
+
+    def __repr__(self):
+        return f"<User {self.username} - {self.email}>"
+    
+class Order(Base):
+
+    ORDER_STATUSES = (
+        ('PENDING', 'pending'),
+        ('IN-TRANSIT', 'in-Transit'),
+        ('DELIVERED', 'delivered'),
+    )
+
+    PIZZA_SIZES = (
+        ('SMALL', 'small'),
+        ('MEDIUM', 'medium'),
+        ('LARGE', 'large'),
+        ('EXTRA_LARGE', 'extra_large'),
+    )
+
+    __tablename__ = 'orders'
+    
+    id = Column(Integer, primary_key=True)
+    quantity = Column(Integer, nullable=False)
+    order_status = Column(ChoiceType(choices = ORDER_STATUSES), default='PENDING')
+    pizza_size = Column(ChoiceType(choices = PIZZA_SIZES), default='SMALL')
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    user = relationship('User', back_populates='orders')
+
+    def __repr__(self):
+        return f"<Order {self.id} - {self.order_status}>"
